0485265cd855a3ab5826cef5e02654bb1bce7201
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-04-03 22:11:33 -0600

    a lot of architecting done

--- src/js/app.js
+++ src/js/app.js
@@ -8,32 +8,49 @@ app.controller('GrossControl', ['$scope', function($scope) {
         {
             id: $scope.id++,
             title: "Gruesome neck pop!",
-            date:
+            user: "Death"
+            date: "January 18, 2016",
             backupImg: "http://cdn.earthporm.com/wp-content/uploads/2014/07/cute-bunnies-tongues-6.jpg",
             image: "https://i.ytimg.com/vi/iQq7cBkjCYU/hqdefault.jpg",
-            votes:
-            description:
-            comments:
+            votes: 5,
+            description: "Danish fontina cheese and biscuits everyone loves. Everyone loves the big cheese rubber cheese parmesan emmental who moved my cheese monterey jack red leicester.",
+            comments: [
+                {
+                    user: "Zoey",
+                    text: "Red leicester stinking bishop cheddar cut the cheese hard cheese cream cheese stilton stinking bishop."
+                }
+            ]
         },
         {
             id: $scope.id++,
-            title:
-            date:
+            title: "Ripped off Toenail",
+            user: "Ben23423"
+            date: "March 16, 2016",
             backupImg: "http://media.treehugger.com/assets/images/2015/07/sea-bunny-jorunna-parva.jpg.662x0_q70_crop-scale.jpg",
-            image:
-            votes:
-            description:
-            comments:
+            image: "https://upload.wikimedia.org/wikipedia/en/8/83/Strong_Bad.png",
+            votes: 0,
+            description: "Cheese triangles cut the cheese cheesecake. Rubber cheese squirty cheese cheese slices parmesan halloumi the big cheese swiss cheesecake. Who moved my cheese caerphilly fromage frais brie cottage cheese.",
+            comments: []
         },
         {
             id: $scope.id++,
-            title:
-            date:
+            title: "Suspension piercings gone wrong",
+            user: "James123"
+            date: "April 1, 2016",
             backupImg: "http://www.rabbit.org/adoption/bunny.jpg",
-            image:
-            votes:
-            description:
-            comments:
+            image: "https://upload.wikimedia.org/wikipedia/en/8/83/Strong_Bad.png",
+            votes: -3,
+            description: "Cottage cheese cheese slices cream cheese queso croque monsieur smelly cheese halloumi feta. Dolcelatte cheese and biscuits croque monsieur halloumi.",
+            comments: [
+                {
+                    user: "Ferdi",
+                    text: "Camembert de normandie stilton pecorino."
+                },
+                {
+                    user: "Ginger",
+                    text: "Camembert de normandie stilton pecorino."
+                }
+            ]
         }
     ]
   $scope.greeting = "Hello World!";

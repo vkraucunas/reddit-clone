048d5fdba048e1c5d33d30558475dac979300aa6
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-04-03 21:35:24 -0600

    add post form written

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,40 @@
 // sample angular code
 
-var app = angular.module('myApp', []);
+var app = angular.module('RedditApp', []);
 
-app.controller('myController', ['$scope', function($scope) {
+app.controller('GrossControl', ['$scope', function($scope) {
+    $scope.id = 1;
+    $scope.posts = [
+        {
+            id: $scope.id++,
+            title: "Gruesome neck pop!",
+            date:
+            backupImg: "http://cdn.earthporm.com/wp-content/uploads/2014/07/cute-bunnies-tongues-6.jpg",
+            image: "https://i.ytimg.com/vi/iQq7cBkjCYU/hqdefault.jpg",
+            votes:
+            description:
+            comments:
+        },
+        {
+            id: $scope.id++,
+            title:
+            date:
+            backupImg: "http://media.treehugger.com/assets/images/2015/07/sea-bunny-jorunna-parva.jpg.662x0_q70_crop-scale.jpg",
+            image:
+            votes:
+            description:
+            comments:
+        },
+        {
+            id: $scope.id++,
+            title:
+            date:
+            backupImg: "http://www.rabbit.org/adoption/bunny.jpg",
+            image:
+            votes:
+            description:
+            comments:
+        }
+    ]
   $scope.greeting = "Hello World!";
 }]);
